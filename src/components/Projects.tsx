a89c69bb03e8e165ceb8d51f956712092d401b3f
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2025-03-04 11:14:00 +0100

    3

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,11 @@ function Projects() {
     return (
       <>
         <h1>Projects</h1>
-        <h3>I build a car using arduino</h3>
+        <p>I built a smart car using an Arduino Uno R4, controlled via WiFi from my phone.  
+            It features two L298N motor drivers and a web-based control interface.  
+            The Arduino itself hosts a lightweight web server that receives and processes movement commands in real time.  
+            One of the biggest challenges was optimizing the response time to ensure smooth and precise movement.</p>
+
         <div className="video-container">
         <video width="250" height="220" controls>
           <source src="Car.mp4" type="video/mp4" />
