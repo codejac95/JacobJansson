e21dc9b85a6c5b6edf3d5c3a7e71a8f1174b50df
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2025-03-04 18:46:12 +0100

    fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ function About() {
           Personally, I consider myself a logical and curious individual, always seeking out fresh and
           innovative solutions to problems. I find backend development to be the most challenging and
           enjoyable aspect of my work, and I aspire to further refine my skills in that domain.
-          Among my various hobbies, I take pleasure in exercise, golfing, cooking delicious meals and computer games.
+          Among my various hobbies, I take pleasure in exercise, racket sports, golfing and computer games.
           I genuinely hope you find this website intriguing. Should you have any inquiries,
           feel free to reach out to me via my LinkedIn. And dont forget to check out my projects and GitHub!
         </p> 
