8baf292c0d5c1e85b7c4e111d7af9027c155e0d0
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2025-03-04 12:58:22 +0100

    base

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,17 +3,22 @@ function Projects() {
     return (
       <>
         <h1>Projects</h1>
-        <p>I built a smart car using an Arduino Uno R4, controlled via WiFi from my phone. <br></br>  
-            It features two L298N motor drivers and a web-based control interface.   <br></br> 
-            The Arduino itself hosts a lightweight web server that receives and processes movement commands in real time.  <br></br>  
+        <h2>WifiCar</h2>
+        <p>I built a smart car using an Arduino Uno R4, controlled via WiFi from my phone. <br/>  
+            It features two L298N motor drivers and a web-based control interface.   <br/> 
+            The Arduino itself hosts a lightweight web server that receives and processes movement commands in real time.<br/>  
             One of the biggest challenges was optimizing the response time to ensure smooth and precise movement.</p>
 
         <div className="video-container">
-        <video width="250" height="220" controls>
-          <source src="Car.mp4"/>
-        </video>
-      </div>
- </>
+          <video width="250" height="220" controls>
+            <source src="Car.mp4"/>
+          </video>
+        </div>
+        <h2>Smart watering</h2>
+        <p>I'm currently working on a automatic watering system for my plants. <br />
+        I use a variety of sensors to measure temperature, humidity, soilmoisture, waterlevels and progress<br/> 
+        </p>
+      </>
     )
   }
   
